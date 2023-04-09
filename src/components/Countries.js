430fe19ed40c28314d0dba436c5bf7e3f88a05c3
IdrissaSy7 <Kolo785@gmail.com>
2023-04-09 03:31:34 +0200

    ajout loader

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -6,11 +6,35 @@ const Flags = () => {
   const [data, setData] = useState([]);
   const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
   const [selectedRadio, setSelectedRadio] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    //   axios
+    //     .get("https://restcountries.com/v3.1/all")
+    //     // .then((res) => setData(res.data));
+    //     .then(function (response) {
+    //       setIsLoading(false);
+    //       setData(response.data);
+    //     })
+    //     .catch(function (error) {
+    //       console.error(error);
+    //     });
+    // }, []);
+
+    const options = {
+      method: "GET",
+      url: "https://restcountries.com/v3.1/all",
+    };
+
     axios
-      .get("https://restcountries.com/v3.1/all")
-      .then((res) => setData(res.data));
+      .request(options)
+      .then(function (res) {
+        setData(res.data);
+        setIsLoading(false);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
   }, []);
 
   console.log(data);
@@ -31,23 +55,33 @@ const Flags = () => {
               <label htmlFor={continent}>{continent}</label>
             </li>
           ))}
-        </div>
-        <div>
-          {selectedRadio && (
-            <button onClick={() => setSelectedRadio("")}>Cancel</button>
-          )}
+          <div>
+            {selectedRadio && (
+              <button onClick={() => setSelectedRadio("")}>Cancel</button>
+            )}
+          </div>
         </div>
       </div>
-      <div>
-        <ul>
-          {data
-            .filter((country) => country.continents[0].includes(selectedRadio))
-            .sort((a, b) => b.population - a.population)
-            .map((country, index) => (
-              <Card key={index} country={country} />
-            ))}
-        </ul>
-      </div>
+      {!isLoading ? (
+        <>
+          <div>
+            <ul>
+              {data
+                .filter((country) =>
+                  country.continents[0].includes(selectedRadio)
+                )
+                .sort((a, b) => b.population - a.population)
+                .map((country, index) => (
+                  <Card key={index} country={country} />
+                ))}
+            </ul>
+          </div>
+        </>
+      ) : (
+        <div className="loader">
+          <i className="fa-solid fa-gear fa-spin"></i>
+        </div>
+      )}
     </div>
   );
 };
